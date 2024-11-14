4a1e7851e02428e56f15d3c892d8c2a605582508
unknown <you@example.com>
2024-11-14 19:14:47 +0800

    wad

--- main/sign_page.py
+++ main/sign_page.py
@@ -6,37 +6,32 @@ class Sign_page():
         self.surf = surf
         self.signed = signed
         self.started = started
-        self.ined = ined
         self.name = Intput_box(screen,600,500,'name')
-        self.password = Intput_box(screen,600,500,'password')
-    def sign_page(self):
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                ask_quetion('quit','do you want to quit the game?')
-            if self.ined == False:
-                if self.signed == True: 
-                    self.surf.fill(black)
-                    pygame.draw.rect(screen,orange,points[15])
-                    pygame.draw.circle(screen,black,points[2],150)
-                    pygame.draw.circle(screen,black,points[3],150)
-                    pygame.draw.rect(screen,black,points[4])
-                    pygame.draw.circle(screen,green,points[5],130)
-                    pygame.draw.circle(screen,green,points[6],130)
-                    pygame.draw.rect(screen,green,points[7])
-                    self.surf.blit(bgtitle,points[1])
-                    self.surf.blit(title,points[0])
-                    self.surf.blit(cardback,points[12]) 
-                    self.name.draw()
-                    self.password.draw()
-                    self.name.interact
-                    self.password.interact
-                elif self.signed == False:
-                    self.surf.fill(orange)
-                    self.surf.blit(button_image07,points[13])
-                    self.surf.blit(button_image08,points[14])
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if 100 < pygame.mouse.get_pos()[0] < 796 and 100 < pygame.mouse.get_pos()[1] < 455:
-                            self.signed = True
-                            login()
-                        if 900 < pygame.mouse.get_pos()[0] < 1596 and 100 < pygame.mouse.get_pos()[1] < 495:
-                            self.signed = True
+        self.password = Intput_box(screen,600,600,'password')
+    def sign_page(self,event):
+        if self.signed == True: 
+            self.surf.fill(black)
+            pygame.draw.rect(screen,orange,points[15])
+            pygame.draw.circle(screen,black,points[2],150)
+            pygame.draw.circle(screen,black,points[3],150)
+            pygame.draw.rect(screen,black,points[4])
+            pygame.draw.circle(screen,green,points[5],130)
+            pygame.draw.circle(screen,green,points[6],130)
+            pygame.draw.rect(screen,green,points[7])
+            self.surf.blit(bgtitle,points[1])
+            self.surf.blit(title,points[0])
+            self.surf.blit(cardback,points[12]) 
+            self.name.draw()
+            self.password.draw()
+            self.name.interact
+            self.password.interact
+        elif self.signed == False:
+            self.surf.fill(orange)
+            self.surf.blit(button_image07,points[13])
+            self.surf.blit(button_image08,points[14])
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 100 < pygame.mouse.get_pos()[0] < 796 and 100 < pygame.mouse.get_pos()[1] < 455:
+                    self.signed = True
+                    login()
+                if 900 < pygame.mouse.get_pos()[0] < 1596 and 100 < pygame.mouse.get_pos()[1] < 495:
+                    self.signed = True
--- main/startgame.py
+++ main/startgame.py
@@ -1,8 +1,12 @@
 from refresh_system import *
 get_cards()
+sp = Sign_page(screen,signed,started)
 while True:
-    sp = Sign_page(screen,signed,started)
-    sp.sign_page()
-    
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            ask_quetion('quit','do you want to quit right now?')
+        
+        sp.sign_page(event)
+
     pygame.display.flip()
 
