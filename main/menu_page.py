bd46cea5b768504481d444e9358a56eb01a4d38a
felixyu2007 <143047471+felixyu2007@users.noreply.github.com>
2024-11-18 00:33:22 +0800

    Update menu_page.py

--- main/menu_page.py
+++ main/menu_page.py
@@ -3,6 +3,8 @@ from sign_page import *
 class Menu():
     def __init__(self,surf,userinfomation,money):
         self.userinfo_rect = pygame.Rect(10,10,300,100)
+        self.rule_rect = pygame.Rect(800,440,320,100)
+        self.rule_slogan = 
         self.surf = surf
     def draw_menu(self):
         self.surf.blit(background_image,(0,0))
