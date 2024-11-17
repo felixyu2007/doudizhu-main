92fac2f795f83353e019a9948210bbb3eba81160
unknown <you@example.com>
2024-11-17 19:03:00 +0800

    nikj

--- main/signin_and_login_system.py
+++ main/signin_and_login_system.py
@@ -4,12 +4,12 @@ def sign_up(usertextname: str,usertextpassword: str):
     usp = usertextpassword
     usm = 10000
     usinfo = {'username':usn,'userpassword':usp,'userfunding':usm}
-    with open('player_info.json','w') as opw:
-        opw = str(opw)
+    with open('main\player_info.json',O_WRONLY) as opw:
         json.dump(usinfo,opw)
 
 def login():
     pass
+
 def get_userinfo():
     pass
 
