8be0dbe103c05da9b538e0b72ac2d27ae6d3658b
felixyu2007 <143047471+felixyu2007@users.noreply.github.com>
2024-11-23 11:30:27 +0800

    dyj

--- main/menu_page.py
+++ main/menu_page.py
@@ -4,7 +4,7 @@ class Menu():
     def __init__(self,surf,userinfomation,money):
         self.userinfo_rect = pygame.Rect(10,10,300,80)
         self.rule_rect = pygame.Rect(800,440,320,100)
-        self.rule_slogan_size = pygame.font.Font(None,80)
+        self.rule_slogan_size = pygame.font.Font(None,120)
         self.rule_slogan = self.rule_slogan_size.render('RULE',True,black)
         self.surf = surf
     def draw_menu(self,event):
