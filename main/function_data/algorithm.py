d534ef75fe960f1f040614dfee5ea583092dff8c
felixyu2007 <you@example.com>
2024-12-30 19:45:34 +0800

    asd

--- main/function_data/algorithm.py
+++ main/function_data/algorithm.py
@@ -6,6 +6,9 @@ from function_data.button import *
 class Game_algorithm():
     def __init__(self,screen):
         self.surf = screen
+        self.round = 0
+        self.price = 0
+        #创建各种牌组和字典（地主牌3张，3个玩家各17张）
         poker_image_path = r'PNG-cards-1.3'
         imgs = os.listdir(poker_image_path)
         self.poker = {}
@@ -15,44 +18,47 @@ class Game_algorithm():
         self.user_choosed_poker = {}
         self.choosed_poker_cache = {}
         self.choose_poker = []
-        self.round = 0
-        self.price = 0
+        #调用先前写好的按钮函数
         self.increase_btn = Button(self.surf,1200,650,'increase')
         self.decrease_btn = Button(self.surf,1200,750,'decrease')
         self.bet_button = Button(self.surf,1450,750,'bet')
         self.grab_button = Button(self.surf,1450,850,'be the landlord')
-        self.unrob_button = Button(self.surf,1450,1000,'be the people')
+        self.unrob_button = Button(self.surf,1450,900,'be the people')
+        self.pass_btn = Button(self.surf,1200,650,'pass')
+        self.play_btn = Button(self.surf,1200,650,'pass')
+        #要把下注的金额绘制出来
         self.pricetext = pygame.font.Font(None,100)
         self.priceshow = self.pricetext.render(''.join(str(self.price)),True,(255,255,255))
         self.card_start_point = [450,750]
         self.position = 0
         self.card_blit_point = {}
         self.choosen = False
+        #往牌组里随机抽出牌然后分别放入（地主牌，3个玩家各17张）
         for p in imgs:
             baba = os.path.dirname(os.path.abspath(p))
             self.haha = os.path.join(baba,poker_image_path+'\\'+p)
             self.image_path = pygame.image.load(self.haha)
             poker_dict = {p:self.image_path}
             self.poker.update(poker_dict)
-
+        #地主牌的
         for a in range(3):
             self.choose_poker = random.choice(list(self.poker.keys()))
             self.choosed_poker_cache = {self.choose_poker:self.poker[self.choose_poker]}
             self.choosed_dizhu_poker.update(self.choosed_poker_cache)
             del self.poker[self.choose_poker]
-        
+        #3个玩家的
         for b in range(17):
-            
+            #ai的
             self.choose_poker = random.choice(list(self.poker.keys()))
             self.choosed_poker_cache = {self.choose_poker:self.poker[self.choose_poker]}
             self.choosed_poker01.update(self.choosed_poker_cache)
             del self.poker[self.choose_poker]
-
+            #ai的
             self.choose_poker = random.choice(list(self.poker.keys()))
             self.choosed_poker_cache = {self.choose_poker:self.poker[self.choose_poker]}
             self.choosed_poker02.update(self.choosed_poker_cache)
             del self.poker[self.choose_poker]
-
+            #玩家的
             self.choose_poker = random.choice(list(self.poker.keys()))
             self.choosed_poker_cache = {self.choose_poker:self.poker[self.choose_poker]}
             self.user_choosed_poker.update(self.choosed_poker_cache)
@@ -104,7 +110,9 @@ class Game_algorithm():
                 print(self.card_blit_point)
         else:
             Game_algorithm.draw_cards(self,event,mouseevent)
-            
+            passround = self.pass_btn.clickbutton(mouseevent,event)
+            if passround:
+                self.round += 1
 
     def draw_cards(self,event,mouseevent):
         for c in self.user_choosed_poker.values():
@@ -113,9 +121,6 @@ class Game_algorithm():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.image_scale.collidepoint(mouseevent):
                     self.card_blit_point[c] = [self.card_blit_point[c][0],700]
-            if event.type == pygame.MOUSEBUTTONUP:
-                if self.image_scale.collidepoint(mouseevent):
-                    self.card_blit_point[c] = [self.card_blit_point[c][0],750]
             
     def alorithm(self):
         None
