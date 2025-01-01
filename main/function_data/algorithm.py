5fc3d844415af856f080b40e1a41b3c925a7bba8
felixyu2007 <143047471+felixyu2007@users.noreply.github.com>
2025-01-01 23:08:40 +0800

    Update algorithm.py

--- main/function_data/algorithm.py
+++ main/function_data/algorithm.py
@@ -117,7 +117,11 @@ class Game_algorithm():
                 Game_algorithm.ai_alorithm(self)
             ans = Game_algorithm.check_hand(self)
             if ans:
-                Game_algorithm.ai_alorithm(self)
+                play = self.play_btn.clickbutton(mouseevent,event)
+                if play:
+                    Game_algorithm.ai_alorithm(self)
+            else:
+                pass
     def draw_cards(self,event,mouseevent):
         for c in self.user_choosed_poker.values():
             self.image_scale = pygame.Rect(self.card_blit_point[c][0],self.card_blit_point[c][1],49,145)
