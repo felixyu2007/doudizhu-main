638b8e2cb4edc8861a54687eefb11aa91f1f7ada
felixyu2007 <143047471+felixyu2007@users.noreply.github.com>
2025-01-18 13:28:11 +0800

    dfg

--- main/function_data/signin_and_login_system.py
+++ main/function_data/signin_and_login_system.py
@@ -4,23 +4,30 @@ class Signin_and_login_method():
     def __init__(self):
         self.path = 'player_infomation\player_info.json'
         self.data = ''
+    def load_account(self):
+        try:
+            with open(self.path,mode='r') as opr:
+                self.data = {'users':[{user['username']:user for user in json.load(opr)}]}
+                print(self.data)
+        except:
+            self.data = {}
     def create_account(self,username,password):
-        with open(self.path,mode='r') as opr:
-            self.data = json.load(opr)
-            if username in self.data:
-                function_data.bgdata.ask_quetion('sign_error','account existed')
-                return False
-            if username not in self.data:
-                Signin_and_login_method.save_account(self,username,password)
-                function_data.bgdata.ask_quetion('login?','account created')
-                return True
+        if username in self.data:
+            function_data.bgdata.ask_quetion('sign_error','account existed')
+            return False
+        elif username not in self.data:
+            Signin_and_login_method.save_account(self,username,password)
+            function_data.bgdata.ask_quetion('login?','account created')
+            return True
 
     def login(self):
         pass
+
     def save_account(self,username,password):
-        with open(self.path,mode='w+') as opw:
-            cache = {username:password}
-            json.dump(cache,opw)
+        with open(self.path,mode='a') as opw:
+            cache = {'username':username,'password':password}
+            json.dump(cache,opw,indent=4,skipkeys=(',',':'))
+
     def delete_account(self,target):
         with open(self.path,mode='r+') as opr:
             self.data = json.load(opr)
@@ -30,7 +37,6 @@ class Signin_and_login_method():
                 function_data.bgdata.ask_quetion('sign_error','account not existed')
                 return False
     def get_userinfo(self):
-        with open(self.path,mode='r') as opr:
-            self.data = json.load(opr)
-            
+        pass
+
             
